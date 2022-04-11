36a6d0a8d367a65ec9184113fbdedbe0f170eb71
MarkuvinesWS <102358282+MarkuvinesWS@users.noreply.github.com>
2022-04-11 13:40:09 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='title'>React+TypeScript!</h1>
+        <img className='pic' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="tree"/>
+        <p className='paragraph'><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In natus possimus qui reprehenderit tenetur totam. Adipisci beatae distinctio enim excepturi expedita incidunt iusto necessitatibus porro provident, ratione repudiandae soluta vel. A accusantium ad beatae earum error et ipsum magni provident, quos ratione sequi temporibus, vero. A aliquid assumenda culpa cumque cupiditate, dicta ducimus expedita laboriosam possimus praesentium, quidem recusandae, reiciendis! </span><span>A culpa, dolorum, fuga laboriosam minus mollitia nesciunt obcaecati, provident quasi repellat tempore veniam. Error ipsum nulla qui quisquam recusandae! Architecto cum itaque odit repellat reprehenderit similique sunt, temporibus unde.</span><span>Delectus, doloribus iure molestiae quaerat quasi, quis quod sed sit suscipit totam ullam ut veritatis? Dolores explicabo fuga qui totam vel! Eaque expedita nihil numquam officiis quam quisquam veniam voluptatum.</span><span>Ab accusamus, aliquid aspernatur beatae corporis culpa deserunt dignissimos eligendi et ex facere illo maxime necessitatibus nostrum nulla obcaecati quisquam, repellendus saepe sint sit sunt totam velit vitae voluptatem voluptates.</span></p>
+    </div>
+  );
+}
+
+export default App;
